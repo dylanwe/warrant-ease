c38f5c0bfdd047d9ea7e106b7425d9d93722e6aa
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-05-04 15:03:55 +0200

    added product entity class

--- backend/src/main/kotlin/com/warrantychecker/backend/models/Product.kt
+++ backend/src/main/kotlin/com/warrantychecker/backend/models/Product.kt
@@ -1,3 +1,17 @@
 package com.warrantychecker.backend.models
 
-data class Product()
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import java.util.Date
+
+@Entity
+data class Product(
+	@Id
+	val id: Long,
+	val name: String,
+	val store: String,
+	val notes: String,
+	val buyDate: Date,
+	val expirationDate: Date,
+	val reminderDate: Date
+)
