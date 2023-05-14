fc0ba4ec3a1161c53581eeffce9d487ab2c1af69
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-05-14 19:15:12 +0200

    added bottom navigation

---
+++ android_app/app/src/main/java/com/warrantease/androidapp/presentation/ui/components/BottomNav.kt
@@ -0,0 +1,77 @@
+package com.warrantease.androidapp.presentation.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
+import com.ramcosta.composedestinations.utils.currentDestinationAsState
+import com.ramcosta.composedestinations.utils.startDestination
+import com.warrantease.androidapp.R
+import com.warrantease.androidapp.presentation.ui.NavGraphs
+import com.warrantease.androidapp.presentation.ui.destinations.HomeScreenDestination
+import com.warrantease.androidapp.presentation.ui.destinations.SettingsScreenDestination
+import com.warrantease.androidapp.presentation.ui.destinations.WarrantiesScreenDestination
+import com.warrantease.androidapp.presentation.ui.theme.AppTheme
+
+enum class BottomBarDestination(
+    val direction: DirectionDestinationSpec,
+    @DrawableRes val icon: Int,
+    @StringRes val label: Int
+) {
+    Home(HomeScreenDestination, R.drawable.outline_home_24, R.string.nav_home_label),
+    Warranties(
+        WarrantiesScreenDestination,
+        R.drawable.baseline_format_list_bulleted_24,
+        R.string.nav_warranties_label
+    ),
+    Settings(
+        SettingsScreenDestination,
+        R.drawable.outline_settings_24,
+        R.string.nav_settings_label
+    ),
+}
+
+@Composable
+fun BottomNav(
+    navController: NavController,
+) {
+    val currentDestination = navController.currentDestinationAsState().value
+        ?: NavGraphs.root.startDestination
+
+    BottomAppBar(
+        containerColor = AppTheme.white,
+        modifier = Modifier.shadow(elevation = 20.dp)
+    ) {
+        BottomBarDestination.values().forEach { destination ->
+            NavigationBarItem(
+                selected = currentDestination == destination.direction,
+                onClick = { navController.navigate(destination.direction) },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = destination.icon),
+                        contentDescription = stringResource(id = destination.label)
+                    )
+                },
+                label = { Text(text = stringResource(id = destination.label)) },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = AppTheme.primary100,
+                    selectedIconColor = AppTheme.neutral600,
+                    selectedTextColor = AppTheme.neutral600,
+                    unselectedIconColor = AppTheme.neutral600
+                )
+            )
+        }
+    }
+}
--- android_app/app/src/main/java/com/warrantease/androidapp/presentation/ui/warranties/WarrantiesScreen.kt
+++ android_app/app/src/main/java/com/warrantease/androidapp/presentation/ui/warranties/WarrantiesScreen.kt
@@ -1,7 +1,13 @@
-package com.warrantease.androidapp.presentation.ui.home
+package com.warrantease.androidapp.presentation.ui.warranties
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -20,7 +26,7 @@ import org.koin.androidx.compose.koinViewModel
 @Destination
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(
+fun WarrantiesScreen(
     navController: NavController,
     viewModel: WarrantyViewModel = koinViewModel()
 ) {
@@ -45,7 +51,7 @@ fun HomeScreen(
                 style = MaterialTheme.typography.titleMedium,
                 fontSize = 30.sp
             )
-            Text(text = viewModel.hello)
+            Text(text = "Warranties")
             if (example == null) {
                 Text(text = "Loading...")
             } else {
@@ -54,6 +60,7 @@ fun HomeScreen(
 
             Spacer(modifier = Modifier.height(10.dp))
             Text(text = user.email ?: "")
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
