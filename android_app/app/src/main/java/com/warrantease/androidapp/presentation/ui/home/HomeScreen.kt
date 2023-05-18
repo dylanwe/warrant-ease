c33ff0e8c14f4fa248760f0e9ba46f76312cac67
Dylan Weijgertze <dylan.weijgertze@hva.nl>
2023-05-18 10:13:25 +0200

    updated the overview look

--- android_app/app/src/main/java/com/warrantease/androidapp/presentation/ui/home/HomeScreen.kt
+++ android_app/app/src/main/java/com/warrantease/androidapp/presentation/ui/home/HomeScreen.kt
@@ -1,11 +1,14 @@
 package com.warrantease.androidapp.presentation.ui.home
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -13,7 +16,10 @@ import androidx.navigation.NavController
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ramcosta.composedestinations.annotation.Destination
+import com.warrantease.androidapp.R
 import com.warrantease.androidapp.presentation.ui.components.BottomNav
+import com.warrantease.androidapp.presentation.ui.components.WarrantPreview
+import com.warrantease.androidapp.presentation.ui.theme.AppTheme
 import com.warrantease.androidapp.presentation.viewmodel.WarrantyViewModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -29,6 +35,14 @@ fun HomeScreen(
     val user = Firebase.auth.currentUser!!
 
     Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Home") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = AppTheme.white
+                )
+            )
+        },
         bottomBar = { BottomNav(navController) }
     ) { innerPadding ->
         Column(
@@ -39,21 +53,33 @@ fun HomeScreen(
                 end = 18.dp
             )
         ) {
-            Text(
-                text = "Hello, ${user.displayName}",
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.titleMedium,
-                fontSize = 30.sp
-            )
-            Text(text = viewModel.hello)
-            if (example == null) {
-                Text(text = "Loading...")
-            } else {
-                Text(text = example!!.toString())
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.outline_wb_sunny_24),
+                    contentDescription = "time icon",
+                    tint = AppTheme.neutral500,
+                    modifier = Modifier.size(18.dp)
+                )
+                Text(text = "Good Morning,", fontSize = 16.sp, color = AppTheme.neutral500)
             }
+            Text(
+                text = "${user.displayName}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
 
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(text = user.email ?: "")
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+                for (i in 0..4) {
+                    item {
+                        WarrantPreview()
+                    }
+                }
+            }
         }
     }
 }
